058c948918218dc7bf11260409205f4040936ffd
Taylor Trimble <t.trim@me.com>
2013-07-30 22:11:36 -0700

    Ridiculously simple playing with pq

--- goserve.go
+++ goserve.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"net/http"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, Internet!")
+func sayHello(name string) http.HandlerFunc {
+	helloPhrase := fmt.Sprintf("Hello, %v!", name)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, helloPhrase)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", sayHello)
+	db, err := sql.Open("postgres", "dbname=fido-research sslmode=disable")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	defer db.Close()
+
+	var name string
+	db.QueryRow("SELECT name FROM users").Scan(&name)
+
+	http.HandleFunc("/", sayHello(name))
 	http.ListenAndServe(":4646", nil)
 }
